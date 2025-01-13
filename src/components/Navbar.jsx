7bea1ab610cae5056093e7893a115c03aa8ebd01
nithin-sivakumar <nithinsgayathri@gmail.com>
2025-01-13 17:41:12 +0530

    Navbar responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoIosMenu } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+  const [isOpenNav, setIsOpenNav] = useState(false);
+
   const links = [
     { id: 1, content: "Services", href: "#services" },
     { id: 2, content: "Case Studies", href: "#case-studies" },
@@ -12,7 +16,7 @@ const Navbar = () => {
   return (
     <nav
       id="top"
-      className="sticky w-full px-14 py-10 flex items-center justify-between bg-borderColor/90 z-50"
+      className="sticky w-full p-6 xl:px-14 xl:py-10 flex items-center justify-between bg-borderColor/90 z-50"
     >
       <div className="flex items-center justify-center cursor-pointer group gap-4">
         <img
@@ -23,7 +27,7 @@ const Navbar = () => {
         <span className="text-3xl font-semibold">Sigmoid</span>
       </div>
 
-      <div className="flex items-center justify-center gap-[40px]">
+      <div className="hidden xl:flex items-center justify-center gap-[40px]">
         <ul className="flex items-center justify-center gap-[40px]">
           {links.map((link) => (
             <a
@@ -39,6 +43,34 @@ const Navbar = () => {
           Request a quote
         </button>
       </div>
+      <div
+        onClick={() => setIsOpenNav(true)}
+        className="flex xl:hidden text-[40px] cursor-pointer"
+      >
+        <IoIosMenu />
+      </div>
+
+      {isOpenNav && (
+        <div className="absolute w-full h-screen bg-white opacity-90 top-0 right-0 transition-all duration-200">
+          <div
+            onClick={() => setIsOpenNav(false)}
+            className="cursor-pointer absolute top-10 right-10 text-[50px]"
+          >
+            <IoClose />
+          </div>
+          <div className="w-full h-full flex flex-col items-center justify-center">
+            {links.map((link) => (
+              <a
+                href={link.href}
+                key={link.id}
+                className="cursor-pointer hover:bg-primary hover:border-b-[1px] hover:border-black px-2 py-1 transition-all duration-200 text-[50px] gap-2"
+              >
+                {link.content}
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
