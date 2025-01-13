1d9ed9b76da8e6f378dc6b046c195f727a39b21c
nithin-sivakumar <nithinsgayathri@gmail.com>
2025-01-13 17:52:50 +0530

    hero responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="mt-[2rem] pl-14 pr-6 flex items-center justify-center gap-4">
-      <div className="flex-1 flex flex-col items-start justify-center gap-[35px]">
-        <h2 className="font-medium text-[60px] max-w-[32rem]">
+    <section className="mt-[2rem] px-6 xl:pl-14 xl:pr-6 flex items-center justify-center gap-4">
+      <div className="flex-1 flex flex-col xl:items-start items-center justify-center gap-[35px]">
+        <h2 className="font-medium text-[36px] xl:text-[60px] max-w-[32rem] xl:text-left text-center">
           Navigating the digital landscape for success
         </h2>
-        <p className="font-normal text-[20px]">
+        <p className="font-normal text-[20px] xl:text-left text-center">
           Our digital marketing agency helps businesses grow and succeed online
           through a range of services including SEO, PPC, social media
           marketing, and content creation.
@@ -17,7 +17,7 @@ const Hero = () => {
         </button>
       </div>
 
-      <div className="flex-1 flex">
+      <div className="flex-1 xl:flex hidden">
         <img className="flex-1" src="/Illustration.png" alt="banner" />
       </div>
     </section>
