7873ff432e196442052e24fd319121eae6f66bea
nithin-sivakumar <nithinsgayathri@gmail.com>
2025-01-13 18:23:11 +0530

    Action responsive

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const Action = () => {
   return (
-    <section className="px-[6.25rem] pr-10 my-10 flex gap-6">
-      <div className="flex-1 flex flex-col items-start justify-center gap-[26px]">
-        <h4 className="font-medium text-[30px]">Let's make things happen</h4>
-        <p className="text-[18px]">
+    <section className="px-6 xl:px-[6.25rem] xl:pr-10 my-10 flex gap-6">
+      <div className="flex-1 flex flex-col items-center xl:items-start justify-center gap-[26px]">
+        <h4 className="font-medium text-center xl:text-left text-[30px]">
+          Let's make things happen
+        </h4>
+        <p className="text-[18px] text-center xl:text-left">
           Contact us today to learn more about how our digital marketing
           services can help your business grow and succeed online.
         </p>
@@ -14,7 +16,7 @@ const Action = () => {
         </button>
       </div>
 
-      <div className="flex-1 flex items-center justify-center ml-[8rem]">
+      <div className="flex-1 hidden xl:flex items-center justify-center ml-[8rem]">
         <img src={`/action/cta.png`} alt="cta" />
       </div>
     </section>
